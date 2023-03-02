7d4c3ad430f5ca627c44e0d9fc7423c17a0aa1aa
EC2 Default User <ec2-user@ip-172-31-39-227.ec2.internal>
2023-03-02 17:30:48 +0000

    First commit adding TF code and application code

---
+++ terraform code/dev/instances/ec2.tf
@@ -0,0 +1,66 @@
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+}
+
+
+resource "aws_instance" "k8s" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.medium"
+
+  root_block_device {
+    volume_size = 16
+  }
+
+  vpc_security_group_ids = [
+    module.ec2_sg.security_group_id,
+    module.dev_ssh_sg.security_group_id
+  ]
+  iam_instance_profile = "LabInstanceProfile"
+  user_data            = file("${path.module}/user_data.sh")
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    project = "clo835"
+  }
+
+  key_name                = "week5"
+  monitoring              = true
+  disable_api_termination = false
+  ebs_optimized           = true
+}
+
+resource "aws_key_pair" "k8s" {
+  key_name   = "week5"
+  public_key = file("${path.module}/week5.pub")
+}
+
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "appecr"
+  #force_delete         = true
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+resource "aws_ecr_repository" "db" {
+  name                 = "dbecr"
+  #force_delete         = true
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
